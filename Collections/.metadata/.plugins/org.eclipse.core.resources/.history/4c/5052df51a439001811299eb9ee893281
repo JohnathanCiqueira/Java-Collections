7fbd6c80270ca068b9f4e8389f1b83f1dc135af0
Johnathan Ciqueira <jciqueira@annex>
2018-10-08 19:59:56 -0300

    first commit

---
+++ Collections/.metadata/.plugins/org.eclipse.core.resources/.history/4c/5052df51a439001811299eb9ee893281
@@ -0,0 +1,5 @@
+package br.com.alura;
+
+public class TestandoListas {
+
+}
---
+++ Collections/gerenciador-de-cursos/src/br/com/alura/TestaAlunos.java
@@ -0,0 +1,33 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class TestaAlunos {
+
+	public static void main(String[] args) {
+		Set<String> alunos = new HashSet<>();
+		alunos.add("Rodrigo Turini");
+		alunos.add("Alberto Souza");
+		alunos.add("Nico Steppat");
+		alunos.add("Sergio Lopes");
+		alunos.add("Renan Saggio");
+		alunos.add("Mauricio Aniche");
+		
+		boolean pauloMatriculado = alunos.contains("Paulo Silveira");
+		System.out.println(pauloMatriculado);
+		
+		System.out.println(alunos.size());
+		
+		for (String aluno : alunos) {
+			System.out.println(aluno);
+		}
+		
+		System.out.println(alunos);
+		
+		List<String> alunoEmLista = new ArrayList<>(alunos);
+		
+	}
+}
